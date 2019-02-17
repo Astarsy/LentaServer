1351b9ab8171713715359b4995b5b935e7c1850d
Astarsy <astarsy@mail.ru>
2019-02-17 22:52:49 +0300

    auth debug

--- engine/Common/config_loc.php
+++ engine/Common/config_loc.php
@@ -1,7 +1,7 @@
 <?php
 // The local configuration file
 return [
-    'AUTH_HOST'=>'https://100tkaney.ru',
+    'AUTH_HOST'=>'http://100tkaney.loc',
     'db'=>[
         'name'=>'u0232960_lenta451888',
         'user'=>'u0232960_lent',
